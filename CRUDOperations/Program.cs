0d0b36fbc8ed5ec2583b9ac2fd0d2c8996c717fa
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-14 15:42:15 +0530

    [Ritvik]Add: Welcome Message

---
+++ CRUDOperations/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUDOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To CRUD operations on ArrayList and Dictionary Collection!!!\n");
+        }
+    }
+}
