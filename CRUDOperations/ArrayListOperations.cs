6518ee5ecde2a89d53a18a8c86000d8303012f11
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-14 16:45:01 +0530

    [Ritvik]Add: Doing CRUD operations in ArrayList

---
+++ CRUDOperations/ArrayListOperations.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace CRUDOperations
+{
+    public class ArrayListOperations
+    {
+        public ArrayList obj = new ArrayList();
+
+        public void AddItems()
+        {
+            obj.Add(10);
+            obj.Add(20);
+            obj.Add(30);
+            obj.Add("This is the fourth value.");
+            obj.Insert(1, "This is the first index");
+        }
+
+        public void DisplayItems()
+        {
+            foreach(var item in obj)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void UpdateItems()
+        {
+            if(obj.Contains(10))
+            {
+                obj.SetRange(0,new ArrayList() {"John"});
+            }
+            Console.WriteLine("\nAfter updating the ArrayLisyt.\n");
+            DisplayItems();
+            Console.WriteLine("\nCapacity: {0}\n",obj.Capacity);
+        }
+
+        public void DeleteItems()
+        {
+            obj.Remove("This is the first index");
+            Console.WriteLine("After the Deleting the item .\n");
+            DisplayItems();
+
+        }
+    }
+}
--- CRUDOperations/Program.cs
+++ CRUDOperations/Program.cs
@@ -1,10 +1,17 @@
 ﻿namespace CRUDOperations
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To CRUD operations on ArrayList and Dictionary Collection!!!\n");
+
+            ArrayListOperations arrayList = new ArrayListOperations();
+
+            arrayList.AddItems();
+            arrayList.DisplayItems();
+            arrayList.UpdateItems();
+            arrayList.DeleteItems();
         }
     }
 }
